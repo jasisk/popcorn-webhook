e91c9fda383328b9243f264027a7731a901eba21
Jean-Charles Sisk <jasisk@gmail.com>
2013-05-08 21:50:54 -0400

    New methods: .list(), .signedList(), .getToken()

--- index.js
+++ index.js
@@ -1,10 +1,2 @@
-var levelup = require("levelup");
-var redis = require("redis");
-var kue = require("kue");
-var rc = require("rc")("popcorn", {
-  port: 6379,
-  server: "127.0.0.1",
-  channel: "popcorn"
-});
-
-kue.app.listen(3000);
+var webhooks = require("./lib/webhooks");
+var jobs = require("./lib/queue");
--- test/webhooks.js
+++ test/webhooks.js
@@ -50,14 +50,12 @@ describe("webhooks", function(){
         should.exist(hook._db);
       });
     });
-
-
   });
 
-  describe(".get()", function(){
+  describe(".list()", function(){
     it("should return an empty array on new db", function(done){
       var hook = webhooks(tmpFile);
-      hook.get(function(err, hooks){
+      hook.list(function(err, hooks){
         should.not.exist(err);
         hooks.should.eql([]);
         hook._db.close();
@@ -72,7 +70,7 @@ describe("webhooks", function(){
         if (err) done(err);
         db.close(function(){
           var hook = webhooks(tmpFile);
-          hook.get(function(err, hooks){
+          hook.list(function(err, hooks){
             var obj = {};
             obj[url] = key;
             should.not.exist(err);
@@ -100,7 +98,7 @@ describe("webhooks", function(){
     it("should add a url", function(done){
       var url = "http://www.paypal.com";
       hook.add(url, function(err){
-        hook.get(function(err, val){
+        hook.list(function(err, val){
           Object.keys(val).should.eql([url]);
           done(err);
         });
@@ -110,7 +108,7 @@ describe("webhooks", function(){
       var url = "http://www.paypal.com";
       hook.add(url, function(err){
         hook.add(url.toUpperCase(), function(err){
-          hook.get(function(err, val){
+          hook.list(function(err, val){
             Object.keys(val).should.eql([url]);
             done(err);
           });
@@ -126,6 +124,36 @@ describe("webhooks", function(){
     });
   });
 
+  describe(".getToken()", function(){
+    var hook;
+    beforeEach(function(){
+      hook = webhooks(tmpFile);
+    });
+    afterEach(function(done){
+      hook._db.close(function(){
+        rimraf(tmpFile, function(err){
+          done(err);
+        });
+      });
+    });
+    it("should return the right token", function(done){
+      var url = "http://www.paypal.com";
+      hook.add(url, function(err, token){
+        hook.getToken(url, function(err, fetchedToken){
+          should.not.exist(err);
+          fetchedToken.should.equal(token);
+          done();
+        });
+      });
+    });
+    it("should error if url doesn't exist", function(done){
+      hook.getToken("http://www.popcorn.com", function(err, fetchedToken){
+        should.exist(err);
+        done();
+      });
+    });
+  });
+
   describe("#sign()", function(){
     it("should calculate the proper hmac", function(){
       var signed = webhooks.sign("http://www.paypal.com", "abcdef", {
@@ -133,15 +161,57 @@ describe("webhooks", function(){
         r:1234567
       });
       signed.should.be.an.object;
-      signed.signature.should.equal("034e2f39fed53242bcdfefc2afc519a9fd28abc3");
+      signed.signature.should.equal("3353da1368c18d1bbec94ec0535802d1e5b1e196");
+    });
+    it("should calculate the proper hmac without params", function(){
+      var signed = webhooks.sign("http://www.paypal.com", "abcdef");
+      signed.should.have.keys("url", "params", "signature");
+      signed.params.should.have.keys("t", "r");
     });
   });
 
   describe("#verify()", function(){
     it("should validate the hmac correctly", function(){
-      var signed = webhooks.sign("http://www.paypal.com", "abcdef");
-      webhooks.verify(signed.url, signed.signature, "abcdef").should.be.true;
-      webhooks.verify(signed.url, signed.signature, "fedcba").should.be.false;
+      var params = {
+        t:1234567,
+        r:1234567
+      };
+      var signed = webhooks.sign("http://www.paypal.com", "abcdef", params);
+      webhooks.verify(signed.url, "abcdef", params, signed.signature).should.be.true;
+      webhooks.verify(signed.url, "abcdef", null, signed.signature).should.be.false;
+      webhooks.verify(signed.url, "fedcba", params, signed.signature).should.be.false;
+    });
+  });
+
+  describe(".signedList()", function(){
+    var hook;
+    var url = "http://www.paypal.com";
+    before(function(done){
+      hook = webhooks(tmpFile);
+      hook.add(url, function(err){
+        done();
+      });
+    });
+    after(function(done){
+      hook._db.close(function(){
+        rimraf(tmpFile, function(err){
+          done(err);
+        });
+      });
+    });
+    it("should return the list properly signed", function(done){
+      hook.signedList(function(err, list){
+        should.not.exist(err);
+        list.should.have.length(1);
+        hook.getToken(list[0].url, function(err, token){
+          var verified = webhooks.verify(list[0].url,
+                                         token,
+                                         list[0].params,
+                                         list[0].signature);
+          verified.should.be.true;
+        });
+        done();
+      });
     });
   });
 
